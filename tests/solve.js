a3d72fa36deccfe5434c49d6b674de6d2232c707
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-04-11 01:45:46 +0800

    refactor: remove solve function and corresponding tests

--- tests/solve.js
+++
@@ -1,8 +0,0 @@
-const solve = (expression) => {
-    let evaluatedExpression = expression.join('');
-    console.log(evaluatedExpression);
-    let result = new Function(`return ${evaluatedExpression}`);
-    console.log(result());
-}
-
-module.exports = [solve, sanitize];
