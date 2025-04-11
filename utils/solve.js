5b1964a61d7f549d9177e3e254adbe3d562b9102
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-04-12 04:50:09 +0800

    refactor: enhance dynamicSolve function to improve expression evaluation logic

--- utils/solve.js
+++ utils/solve.js
@@ -8,8 +8,35 @@ export const solveFlatInPEMDAS = (expression) => {
 
 }
 
-export const dynamicSolve = () => {
-        
+export const dynamicSolve = (expression, renderResult) => {
+        if (expression.length < 3) return;
+
+        if (
+                expression[0].type !== 'number' && 
+                expression[1].type !== 'operator' &&
+                expression[2].type !== 'number'
+        ) 
+        { return; }
+
+        let firstThree = expression.slice(0,3);
+
+        let operand1 = parseFloat(firstThree[0].value);
+        let operator = firstThree[1].value;
+        let operand2 = parseFloat(firstThree[2].value);
+
+        let result;
+        switch (operator) {
+            case '+': result = operand1 + operand2; break;
+            case '-': result = operand1 - operand2; break;
+            case '*': result = operand1 * operand2; break;
+            case '/': result = operand1 / operand2; break;
+            default: return;
+        }
+
+        expression.splice(0, 3, {type: 'number', value: result});
+
+        renderResult(result);
+
 }
 
 // module.exports = solve;
